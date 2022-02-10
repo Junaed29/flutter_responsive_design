40de4a78bb94fe648114fb4ecda2dc49ea16af1e
Junaed <junaed1995@gmail.com>
2022-02-10 13:47:04 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'courses_page.dart';
+
+void main() => runApp(const AppWidget());
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Responsive Framework',
+      home: const CoursesPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
