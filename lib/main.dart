8d13373ba023e46a9801df469ea80733a3570c68
Junaed <junaed1995@gmail.com>
2022-02-11 12:16:02 +0600

    ResponsiveBreakpoint added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'courses_page.dart';
 
@@ -10,6 +11,15 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (ctx, widget) => ResponsiveWrapper.builder(
+          ClampingScrollWrapper.builder(context, widget!),
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(350, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(600, name: TABLET),
+            const ResponsiveBreakpoint.resize(800, name: DESKTOP),
+            const ResponsiveBreakpoint.autoScale(1700, name: "XL"),
+          ]),
       title: 'Flutter Responsive Framework',
       home: const CoursesPage(),
       debugShowCheckedModeBanner: false,
